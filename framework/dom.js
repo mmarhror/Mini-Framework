a3b024a87715f00092469a838b4642db6e85ebb1
mmarhror <mohammedmarhror@gmail.com>
2026-06-05 15:42:35 +0100

    init dom's diffing

--- framework/dom.js
+++ framework/dom.js
@@ -1,5 +1,7 @@
+// Creates a Virtual node.
 export function createVnode(tag, attrs = {}, ...children) {
   return {
+    type: "element",
     tag: tag,
     attrs: attrs,
     children: children
@@ -7,14 +9,23 @@ export function createVnode(tag, attrs = {}, ...children) {
       .filter(
         (child) =>
           child !== null && child !== undefined && typeof child !== "boolean",
-      ),
+      )
+      .map((child) => {
+        if (typeof child === "string" || typeof child === "number") {
+          return { type: "text", value: child, dom: null };
+        }
+        return child;
+      }),
   };
 }
 
 // Takes the Vnode and renders it into dom.
 function render(vnode) {
-  if (typeof vnode === "string" || typeof vnode === "number") {
-    return document.createTextNode(vnode);
+  if (vnode.type === "text") {
+    const textNode = document.createTextNode(vnode.value);
+    vnode.dom = textNode;
+
+    return textNode;
   }
 
   // Creating the element in dom.
@@ -42,11 +53,12 @@ function render(vnode) {
     // Regular Attribute
     node.setAttribute(key, value);
   }
-
+  // Append Children.
   vnode.children.forEach((vchild) => {
     node.appendChild(render(vchild));
   });
 
+  vnode.dom = node;
   return node;
 }
 
@@ -55,8 +67,8 @@ let currentVnode = null;
 export function mount(vnode, holderId) {
   if (!currentVnode) {
     initMount(vnode, holderId);
+    currentVnode = vnode;
   } else {
-
   }
 }
 
@@ -73,3 +85,47 @@ function initMount(vnode, holderId) {
   parent.replaceChild(node, holder);
 }
 
+function diff(oldNode, newNode, parent) {
+  if (!newNode) {
+    oldNode.dom.remove();
+    return;
+  }
+
+  if (!oldNode) {
+    const newElem = render(newNode);
+    parent.appendChild(newElem);
+    newNode.dom = newElem;
+    return;
+  }
+
+  const oldElem = oldNode.dom;
+
+  if (!isSameKindNodes(oldNode, newNode)) {
+    const newElem = render(newNode);
+    parent.replaceChild(newElem, oldElem);
+    newNode.dom = newElem;
+    return;
+  }
+
+  const oldAttrs = oldNode.attrs;
+  const newAttrs = newNode.attrs;
+
+  for (const key in oldAttrs) {
+    if (!newAttrs[key]) {
+      oldElem.removeAttribute(key);
+    }
+  }
+
+  for (const key in newAttrs) {
+    const value = newAttrs[key];
+    if (oldAttrs[key] !== newAttrs[key]) {
+      oldElem.setAttribute(key, newAttrs[key]);
+    }
+  }
+}
+
+function isSameKindNodes(n1, n2) {
+  return n1.type === n2.type && n1.tag === n2.tag;
+}
+
+function nodesAttrsCompare(n1, n2) {}
